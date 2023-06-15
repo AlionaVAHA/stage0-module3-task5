0e75be942d0b7ba440bf34ff390d495ee980026b
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 00:45:22 -0400

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-         int result = toBeDivided/5;
-        System.out.println(result);
+         float result = toBeDivided/5;
+        System.out.print(result);
     }
 }
