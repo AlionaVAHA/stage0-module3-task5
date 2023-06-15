2706540b7868c105765fc2d44195142b52c5e181
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 00:42:28 -0400

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-          float seconds = minutes*60;
+          int seconds =(int) minutes*60;
           System.out.print(seconds); 
     }
 }
